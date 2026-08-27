c7b849331acf2d3d92b59cba0a52d1e5bbdad086
Biraj Bhattarai <birajbhattarai299@gmail.com>
2026-08-27 12:14:16 +0545

    support page

--- frontend/src/landing_page/support/CreateTicket.jsx
+++ frontend/src/landing_page/support/CreateTicket.jsx
@@ -1,7 +1,107 @@
 function CreateTicket() {
-    return ( 
-        <div>this is</div>
-     );
+  return (
+    <div className="container">
+      <div className="row">
+        <h1 className="fs-4 mt-5">
+          To create a ticket, select a relevant topic
+        </h1>
+      </div>
+      <div className="row">
+        <div className="col p-1">
+          <h5 className="fs-6 my-5"> <i class="fa-solid fa-circle-plus"></i> Account opening</h5>
+          <ul className="list-unstyled">
+    <li className="lh-lg"><a href="" className="text-decoration-none">Online Account Opening</a></li>
+    <li className="lh-lg"><a href="" className="text-decoration-none">Offline Account Opening</a></li>
+    <li className="lh-lg"><a href="" className="text-decoration-none">Company, Partnership and HUF Account Opening</a></li>
+    <li className="lh-lg"><a href="" className="text-decoration-none">NRI Account Opening</a></li>
+    <li className="lh-lg"><a href="" className="text-decoration-none">Charges at Zerodha</a></li>
+    <li className="lh-lg"><a href="" className="text-decoration-none">Zerodha IDFC FIRST Bank 3-in-1 Account</a></li>
+    <li className="lh-lg"><a href="" className="text-decoration-none">Getting Started</a></li>
+</ul>
+        </div>
+        <div className="col p-1">
+          <h5 className="fs-6 my-5"><i class="fa-solid fa-user"></i> Your BullTrad Account</h5>
+          <ul className="list-unstyled">
+            <li className="lh-lg">
+              <a href="" className="text-decoration-none">
+                Login Credentials
+              </a>
+            </li>
+            <li className="lh-lg">
+              <a href="" className="text-decoration-none">
+                Account Modification and Segment Addition
+              </a>
+            </li>
+            <li className="lh-lg">
+              <a href="" className="text-decoration-none">
+                DP ID and bank details
+              </a>
+            </li>
+            <li className="lh-lg">
+              <a href="" className="text-decoration-none">
+                Your Profile
+              </a>
+            </li>
+            <li className="lh-lg">
+              <a href="" className="text-decoration-none">
+                Transfer and conversion of shares
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="col p-1">
+          <h5 className="fs-6 my-5"><i class="fa-solid fa-chart-column"></i> Your BullTrad Account</h5>
+          <ul className="list-unstyled">
+            <li className="lh-lg">
+              <a href="" className="text-decoration-none">
+                Margin/leverage, Product and Order types
+              </a>
+            </li>
+            <li className="lh-lg">
+              <a href="" className="text-decoration-none">
+                Kite Web and Mobile
+              </a>
+            </li>
+            <li>
+              <a href="" className="text-decoration-none">
+                Trading FAQs
+              </a>
+            </li>
+            <li className="lh-lg">
+              <a href="" className="text-decoration-none">
+                Corporate Actions
+              </a>
+            </li>
+            <li className="lh-lg">
+              <a href="" className="text-decoration-none">
+                Sentinel
+              </a>
+            </li>
+            <li className="lh-lg">
+              <a href="" className="text-decoration-none">
+                Kite API
+              </a>
+            </li>
+            <li className="lh-lg">
+              <a href="" className="text-decoration-none">
+                Pi and other platforms
+              </a>
+            </li>
+            <li className="lh-lg">
+              <a href="" className="text-decoration-none">
+                Stockreports+
+              </a>
+            </li>
+            <li className="lh-lg">
+              <a href="" className="text-decoration-none">
+                GTT
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default CreateTicket;
--- frontend/src/landing_page/support/Hero.jsx
+++ frontend/src/landing_page/support/Hero.jsx
@@ -1,5 +1,39 @@
 function Hero() {
-    return ( <div>this is</div> );
+    return ( 
+        <div id="Support-container-box" className="p-5">
+            <div className="container">
+                <div className="row py-5 d-flex justify-content-between">
+                    <div className="col-auto">
+                        <h3>Support Portal</h3>
+                    </div>
+                    <div className="col-auto">
+                        <p className="me-5 pe-5"><a href="" className="white">Track Ticket</a></p>
+                    </div>
+                </div>
+                <div className="row mb-3">
+                    <div className="col-6">
+                        <h3 className="fs-4">Search for an answer or browse help topics to create a ticket</h3>
+                        <input type="text" className="form-control p-3 my-5 " placeholder="Eg: how do i activate F&O, why is my order getting..."/>
+                        <div>
+                            <a href="" className="small me-4 white">Track account opening</a>
+<a href="" className="small me-4 white">Track segment activation</a>
+<a href="" className="small me-4 white">Intraday margins</a>
+<a href="" className="small me-4 white">Kite user manual</a>
+
+                        </div>
+                    </div>
+                    <div className="col-2"></div>
+                    <div className="col-4">
+                        <h3>Featured</h3>
+                        <ol>
+                            <li><a href="" className="white lh-lg">Current takeovers and Delistings - January 2026</a></li>
+                            <li><a href="" className="white lh-lg">Latest Intraday leverages - MIS & CO</a></li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </div>
+     );
 }
 
 export default Hero;
